62cff95422d5a701cc3495eb446985419328a13c
quentinDyma <quentindevial19@gmail.com>
2021-09-29 09:11:45 +0200

    update: server

--- server/app.js
+++ server/app.js
@@ -2,19 +2,22 @@ const express = require('express');
 const path = require('path');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const app = express();
 
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
 
 mongoose.connect(
   'mongodb+srv://angulardyma:123123123@cluster0-urpjt.gcp.mongodb.net/angulardyma?retryWrites=true&w=majority',
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  },
   function (error) {
     if (error) {
       console.log(error);
