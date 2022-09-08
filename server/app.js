0dd0978dc3c9bf039ff7030cf563483e064dceca
dymafr <contact@dyma.fr>
2022-09-08 14:12:56 +0200

    maj

--- server/app.js
+++ server/app.js
@@ -12,20 +12,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
 
-mongoose.connect(
-  'mongodb+srv://angulardyma:123123123@cluster0-urpjt.gcp.mongodb.net/angulardyma?retryWrites=true&w=majority',
-  {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-  },
-  function (error) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log('Connexion opened to mongodb!');
-    }
-  },
-);
+mongoose.connect('mongodb://localhost:27017/meandyma', (error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Connexion opened to mongodb!');
+  }
+});
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
